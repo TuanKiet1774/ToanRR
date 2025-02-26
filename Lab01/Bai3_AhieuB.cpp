1093ff31efd2a6b84bd616969fd55b2375b6cccf
TuanKiet1774 <167632480+TuanKiet1774@users.noreply.github.com>
2025-02-26 13:36:25 +0700

    Add files via upload

---
+++ Lab01/Bai3_AhieuB.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#define max 1000
+using namespace std;
+
+void Nhap(int a[max], int n)
+{
+	if(n != 0)
+	{
+		cout << "Nhap mang: ";
+		for(int i = 0; i<n; i++)
+			cin >> a[i];
+	}
+}
+
+void Xuat(int a[max], int n)
+{
+	for(int i = 0; i<n; i++)
+		cout << a[i] << " ";
+}
+
+int checkAinB(int pt, int b[max], int m)
+{
+	for(int i = 0; i<m; i++)
+		if(b[i] == pt) return 1;
+	return 0;
+}
+
+void AhieuB(int a[max], int n, int b[max], int m)
+{
+	int kq[max], k=0;
+	for(int i = 0; i<n; i++)
+		if(checkAinB(a[i],b,m) == 0)
+			kq[k++] = a[i];
+	if(k != 0)
+	{
+		cout << "A hieu B: ";
+		Xuat(kq,k); 
+	}
+	else cout << "A hieu B la tap rong";
+}
+
+int main()
+{
+	int a[max], b[max], n, m;
+	cout << "Do dai mang A: ";
+	cin >> n;
+	Nhap(a,n);
+	cout << "Do dai mang B: ";
+	cin >> m;
+	Nhap(b,m);
+	
+	AhieuB(a,n,b,m);
+}
