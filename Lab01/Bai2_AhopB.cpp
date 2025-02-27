46d4847ebb5b67588aef7bd9d0510d957444f0ef
TuanKiet1774 <167632480+TuanKiet1774@users.noreply.github.com>
2025-02-27 09:57:29 +0700

    Update Bai2_AhopB.cpp

--- Lab01/Bai2_AhopB.cpp
+++ Lab01/Bai2_AhopB.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <algorithm>
 #define max 1000
 using namespace std;
 
@@ -13,18 +14,22 @@ void Nhap(int a[max], int n)
 {
 	if(n != 0)
 	{
-		cout << "Nhap mang: ";
 		for(int i = 0; i<n; i++)
 			do cin >> a[i];
 			while (checkAinB(a[i],a,i));
 	}
 }
 
-
 void Xuat(int a[max], int n)
 {
 	for(int i = 0; i<n; i++)
-		cout << a[i] << " ";
+	{
+		if(i == n - 1)
+		{
+			cout << a[i];
+		}
+		else cout << a[i] << " "; 
+	}
 }
 
 void AhopB(int a[max], int n, int b[max], int m)
@@ -39,19 +44,17 @@ void AhopB(int a[max], int n, int b[max], int m)
 			kq[k++] = b[i];
 	if(k != 0)
 	{
-		cout << "A hop B: ";
+		sort(kq,kq+k);
 		Xuat(kq,k);
 	}
-	else cout << "A hop B la tap rong";
+	else cout << "none";
 }
 
 int main()
 {
 	int a[max], b[max], n, m;
-	cout << "Do dai mang A: ";
 	cin >> n;
 	Nhap(a,n);
-	cout << "Do dai mang B: ";
 	cin >> m;
 	Nhap(b,m);
 	
