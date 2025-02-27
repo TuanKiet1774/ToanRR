12fac8c2e71b277853361d438d5aaf8f67af1c9e
TuanKiet1774 <167632480+TuanKiet1774@users.noreply.github.com>
2025-02-27 07:36:31 +0700

    Update Bai2_AhopB.cpp

--- Lab01/Bai2_AhopB.cpp
+++ Lab01/Bai2_AhopB.cpp
@@ -2,27 +2,29 @@
 #define max 1000
 using namespace std;
 
+int checkAinB(int pt, int b[max], int m)
+{
+	for(int i = 0; i<m; i++)
+		if(b[i] == pt) return 1;
+	return 0;
+}
+
 void Nhap(int a[max], int n)
 {
 	if(n != 0)
 	{
 		cout << "Nhap mang: ";
 		for(int i = 0; i<n; i++)
-			cin >> a[i];
+			do cin >> a[i];
+			while (checkAinB(a[i],a,i));
 	}
 }
+
 
 void Xuat(int a[max], int n)
 {
 	for(int i = 0; i<n; i++)
 		cout << a[i] << " ";
-}
-
-int checkAinB(int pt, int b[max], int m)
-{
-	for(int i = 0; i<m; i++)
-		if(b[i] == pt) return 1;
-	return 0;
 }
 
 void AhopB(int a[max], int n, int b[max], int m)
