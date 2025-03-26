2f9510e929404068172d35dab1701d7f14dbd79e
TuanKiet1774 <167632480+TuanKiet1774@users.noreply.github.com>
2025-03-26 21:32:28 +0700

    Add files via upload

---
+++ KiemTra/ToiUu_TH_CH.cpp
@@ -0,0 +1,89 @@
+//To hop chap k cua n
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void DaoChuoi(string &s) 
+{
+    int l = 0;
+    int r = s.size() - 1;
+    while (l < r) 
+    {
+        char temp = s[l];
+        s[l] = s[r];
+        s[r] = temp;
+        l++;
+        r--;
+    }
+}
+
+string Nhan(string n1, int n2) 
+{
+    string kq = "";
+    int du = 0;
+    
+    for (int i = n1.size() - 1; i >= 0; i--) 
+    {
+        int m = (n1[i] - '0') * n2 + du;
+        kq += (m % 10) + '0';
+        du = m / 10;
+    }
+    
+    while (du) 
+	{
+        kq += (du % 10) + '0';
+        du /= 10;
+    }
+    
+    DaoChuoi(kq);
+    return kq;
+}
+
+string ChinhHop(int n, int k) 
+{
+    string kq = "1";
+    for (int i = 0; i < k; i++) 
+	kq = Nhan(kq, n - i);
+    return kq;
+}
+
+string Chia(string n1, int n2) 
+{
+    string kq = "";
+    int du = 0;
+    
+    for (int i = 0; i < n1.size(); i++) 
+    {
+        du = du * 10 + (n1[i] - '0');
+        kq += (du / n2) + '0';
+        du %= n2;
+    }
+    
+    while (kq.size() > 1 && kq[0] == '0') 
+	kq.erase(0, 1); //Xóa 1 ký t? t?i v? trí 0, t?c là lo?i b? ký t? '0' d?u tiên c?a chu?i
+    return kq;
+}
+
+string ToHop(int n, int k)
+{
+    string kq = ChinhHop(n, k);
+    for (int i = 2; i <= k; i++) 
+	kq = Chia(kq, i);
+    return kq;
+}
+
+
+int main() 
+{
+    int n, k;
+    cin >> n >> k;
+    
+    if (k > n || n < 0 || k < 0) 
+		cout << "none"; 
+    else 
+    {
+    	cout << "To hop chap k cua n phan tu: "<<ToHop(n, k) << endl;
+    	cout << "Chinh hop chap k cua n phan tu: " <<ChinhHop(n, k);
+	}
+}
