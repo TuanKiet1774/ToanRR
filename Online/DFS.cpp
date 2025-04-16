36083ad047c5d6007292b3c31faece05d267f663
TuanKiet1774 <167632480+TuanKiet1774@users.noreply.github.com>
2025-04-16 23:35:49 +0700

    Update DFS.cpp

--- Online/DFS.cpp
+++ Online/DFS.cpp
@@ -1,39 +1,36 @@
+//Ứng dụng DFS tìm đường đi từ đỉnh S đến đỉnh T
 #include <iostream>
 #include <stack>
 #include <algorithm> 
 using namespace std;
 
 int G[100][100];
-bool tham[100];
-int cha[100];
+bool tham[100]; //Mảng cho biết đỉnh đó đã đi qua hay chưa
+int cha[100]; //Mảng cho biết đỉnh trước đó của đỉnh hiện tại
 int n;
-
 
 void DFS(int S) 
 {
     stack<int> stk;
     stk.push(S);
 
-    for (int i = 0; i < n; i++) 
-	{
-        tham[i] = false; 
-        cha[i] = -1;
+    for (int i = 0; i < n; i++) {
+        tham[i] = false; //Đánh dấu chưa qua bất kỳ đỉnh nào
+        cha[i] = -1; //Đánh dấu tại đỉnh hiện tại chưa có bất kỳ đỉnh nào phía trước
     }
 
-    while (!stk.empty()) 
-	{
+    while (!stk.empty()){
         int u = stk.top();
         stk.pop();
 
-        if (!tham[u]) 
-		{
+        if (!tham[u]) {
             tham[u] = true;
-            for(int v = n - 1; v >= 0; v--) 
-			{
-                if (G[u][v] && !tham[v]) 
-				{
+		
+            for(int v = n - 1; v >= 0; v--) {
+                if (G[u][v] && !tham[v]) {
                     stk.push(v);
-                    if (cha[v] == -1) cha[v] = u;
+                    if (cha[v] == -1) 
+			    cha[v] = u;
                 }
             }
         }
@@ -43,29 +40,26 @@ void DFS(int S)
 void findWay(int S, int T, int kq[], int k) {
     DFS(S);
 
-    if (!tham[T]) cout << "Khong tim thay duong di tu " << S << " den " << T << ".\n";
-    else
-    {
-    	int ht = T;
-	    while (ht != -1) 
-		{
-	        kq[k++] = ht;
-	        ht = cha[ht];
+    if (!tham[T]) 
+	    cout << "Khong tim thay duong di tu " << S << " den " << T << ".\n";
+    else{
+	    int ht = T;
+	    while (ht != -1) {
+		kq[k++] = ht;
+		ht = cha[ht];
 	    }
 	
 	    reverse(kq, kq + k);
 	    
 	    cout << "Duong di tu " << S << " den " << T << " la: ";
 	    for (int i = 0; i < k; i++)
-	        cout << kq[i] << " ";
+		cout << kq[i] << " ";
 	    cout << endl;
-	}
-		
+    }		
 }
 
-int main() 
-{
-	int S, T;
+int main() {
+    int S, T;
     int kq[100];
     int k = 0;
 
@@ -82,7 +76,18 @@ int main()
     cin >> S;
     cout << "Nhap T: ";
     cin >> T;
-	
-	findWay(S,T,kq,k);
+    findWay(S,T,kq,k);
 }
 
+//Ví dụ:
+// n = 6
+// Ma trận kề: 
+// 0 1 1 0 0 0
+// 1 0 0 1 0 0
+// 1 0 0 1 0 0
+// 0 1 1 0 1 0
+// 0 0 0 1 0 1
+// 0 0 0 0 1 0
+// Đỉnh S = 3
+// Đỉnh T = 5
+// Kết quả: 3 4 5
