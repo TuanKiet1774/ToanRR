cc5fbe27d1b45384ff550d043bbee16a20b296a0
TuanKiet1774 <167632480+TuanKiet1774@users.noreply.github.com>
2025-04-22 22:24:11 +0700

    Update DinhKe.cpp

--- Online/DinhKe.cpp
+++ Online/DinhKe.cpp
@@ -4,29 +4,29 @@ using namespace std;
             
 int main() 
 {
-	int G[MAX][MAX];           
-	int n;
+    int G[MAX][MAX];
+    int n;
 	//cout <<"So dinh: ";
     cin >> n;
     for (int i = 0; i < n; i++)  
         for (int j = 0; j < n; j++)
             cin >> G[i][j];
 
-	int kq[MAX];
-	int k = 0;
+    int kq[MAX];
+    int k = 0;
     int u;
     //cout << "Nhap dinh u: ";
     cin >> u;
     for(int v = 0; v < n; v++)
     	if(G[u][v] != 0)
-    		kq[k++] = v;
+	    kq[k++] = v;
     		
     if(k != 0)
     {
     	//cout << "Cac dinh ke voi dinh " << u << ": ";
     	for(int i = 0; i < k; i++)
     		cout << kq[i] <<" ";
-	}
-	else 
-		cout << "none";
+    }
+    else 
+	cout << "none";
 }
