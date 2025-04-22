796965942fecd3f9d678744b89926125df240751
TuanKiet1774 <167632480+TuanKiet1774@users.noreply.github.com>
2025-04-22 19:26:24 +0700

    Add files via upload

---
+++ Online/DFS.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+#define MAX 100
+
+int G[MAX][MAX];     
+bool tham[MAX];      
+int cha[MAX];        
+int n;               
+
+void DFS(int S) 
+{
+    stack<int> stk;
+    stk.push(S);
+
+    for (int i = 0; i < n; i++) 
+	{
+        tham[i] = false;
+        cha[i] = -1;
+    }
+
+    while (!stk.empty()) 
+	{
+        int u = stk.top();
+        stk.pop();
+
+        if (!tham[u]) 
+		{
+            tham[u] = true;
+            cout << u << " ";
+
+            for (int v = n - 1; v >= 0; v--) {
+                if (G[u][v] && !tham[v]) {
+                    stk.push(v);
+                    if (cha[v] == -1)
+                        cha[v] = u;
+                }
+            }
+        }
+    }
+}
+
+int main() 
+{
+    cin >> n; //So dinh cua do thi
+    for (int i = 0; i < n; i++)  
+        for (int j = 0; j < n; j++)
+            cin >> G[i][j];
+
+    int s;
+    cin >> s; //Dinh bat dau
+
+    DFS(s);
+}
+
